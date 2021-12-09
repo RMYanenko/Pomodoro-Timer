5f076b3b6af0e2f094a5799a5d4103861642c295
RMYanenko <yanenko.r.m@gmail.com>
2021-12-09 16:11:18 +0200

    add fun stop timer

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  workDuration = 1;
+  workDuration = 25;
   breakDuration = 5;
   titleDuration = 'Time to Work!';
   minutes =  this.workDuration - 1;
-  seconds = 10;
-  inrerval: any = 0;
+  seconds = 60;
+  interval: any = 0;
 
   constructor() { }
 
@@ -29,20 +29,28 @@ export class TimerComponent implements OnInit {
     this.minutes = this.workDuration - 1;
   }
 
-  startTime() {
-    this.inrerval = setInterval(() => {
-      counter()
-    }, 1000)
-    const counter = () => {
-      this.seconds --;
-      if (this.seconds == -1) {
-        this.minutes--
-        this.seconds = 60;
-        if (this.minutes == -1) {
-          this.titleDuration = 'Time to Break!'
-        }
+  counter = () => {
+    this.seconds --;
+    if (this.seconds == -1) {
+      this.minutes--
+      this.seconds = 60;
+      if (this.minutes == -1) {
+        this.titleDuration = 'Time to Break!'
       }
     }
+    if(this.seconds < 10) {
+      // this.seconds.toString().padStart(2, '0');
+    }
+  }
+
+  startTime() {
+    this.interval = setInterval(() => {
+      this.counter();
+    }, 1000)
+  }
+
+  stopTime() {
+    clearInterval(this.interval);
   }
 
 }
